5f7597a5129cbb7c590f4dc086b40b8ad5823918
mgapp <mgapp@MIKEGYO>
2025-06-03 18:13:25 -0400

    Add project files.

---
+++ ConditionStatementHW/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ConditionStatementHW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your name: ");
+            string? firstName = Console.ReadLine();
+            bool isValid = int.TryParse(firstName, out int numberName);
+            while (firstName == "" || isValid == true)
+            {
+                Console.WriteLine("Please enter your actual name.");
+                Console.Write("What is your name: ");
+                firstName = Console.ReadLine();
+                if (firstName != "")
+                {
+                    isValid = int.TryParse(firstName, out numberName);
+                    if (isValid == true)
+                    {
+                        Console.WriteLine("Please enter a actual name.");
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            };
+            if (firstName.ToLower() == "tim" ||  firstName.ToLower() == "timothy")
+            {
+                Console.WriteLine("Hello Professor");
+            }
+            else
+            {
+                Console.WriteLine("Hello Student.");
+            }
+
+        }
+    }
+}
